ebacb3dc85f098655032f0712a8884f8819bc035
AK Rasel <raselak101010@gmail.com>
2023-10-18 18:13:28 +0600

    express Setting Up

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+
+app.get("/", (req, res) => {
+    res.send("Crud is running...");
+  });
+  
+  
+
+  
+app.listen(port, () => {
+    console.log(`Simple Crud is Running on port ${port}`);
+  });
+  
+  
